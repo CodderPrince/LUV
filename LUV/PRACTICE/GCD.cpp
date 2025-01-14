4914dcda511fe62cc12f6918b9cc03d144763a7d
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-15 00:32:18 +0600

    Create GCD.cpp

---
+++ LUV/PRACTICE/GCD.cpp
@@ -0,0 +1,28 @@
+/*
+You are given an array A of integers of size N. You will be given Q queries where
+each query is represented by two integers L, R. You have to find the gcd(Greatest
+Common Divisor) of the array after excluding the part from range L to R inclusive
+(1 Based indexing). You are guaranteed that after excluding the part of the array
+remaining array is non empty
+Input
+• First line of input contains an integer T denoting number of test cases.
+• For each test case, first line will contain two space separated integers N, Q.
+• Next line contains N space separated integers denoting array A.
+• For next Q lines, each line will contain a query denoted by two space
+separated integers L, R.
+Output
+For each query, print a single integer representing the answer of that query.
+
+Constraints:
+Subtask 1:
+2 <=T <=100
+1 <=Q <=N
+1 <= A[i] <=10^5
+
+Subtask 2:
+2 <=T <=10^5
+1 <=Q <=N
+1 <= A[i] <=10^5
+
+*/
+
